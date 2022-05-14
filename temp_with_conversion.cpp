2b138fdbd5ee4c8f7d45af4cd0dcd95d4bc414d0
samarthk18 <90502630+samarthk18@users.noreply.github.com>
2022-05-14 06:05:39 +0530

    Update temp_with_conversion.cpp

--- temp_with_conversion.cpp
+++ temp_with_conversion.cpp
@@ -1,1 +1,26 @@
-
+#include <dht.h>
+#define dht_apin A0
+dht DHT;
+float temp;
+float fahren;
+ 
+void setup(){
+ 
+  Serial.begin(9600);
+  delay(500);//Delay to let system boot
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  delay(1000);//Wait before accessing Sensor
+ 
+}//end "setup()"
+ 
+void loop()
+{
+  DHT.read11(dht_apin);
+  Serial.print("temperature = ");
+  temp=Serial.print(DHT.temperature); 
+  Serial.println("C  ");
+  fahren=((temp*1.8)+32);
+  Serial.print(fahren);
+  Serial.println("F ");
+  delay(5000);//Wait 5 seconds before accessing sensor again.
+}
